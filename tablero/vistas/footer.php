2db50fce9af43a4fb5ebb4996fc443807679a779
Alfonso Sánchez <a@asx.mx>
2025-09-28 23:25:23 -0600

    limpieza

--- tablero/vistas/footer.php
+++
@@ -1,7 +0,0 @@
-<html>
-
-     <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.slim.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!--<p class="text-center text-secondary">Tablero de Actividades de Grupo Ureña Funerarios - Sistema Realizado por LI. Luis Raúl Vargas Aguirre</p>-->
-</html>
