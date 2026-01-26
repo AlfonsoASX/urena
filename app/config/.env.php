cca8cdaf098a26c890a5c621c10155d2408323b1
Alfonso Sánchez <a@asx.mx>
2026-01-26 01:18:23 -0600

    cambio de conexión

--- app/config/.env.php
+++ app/config/.env.php
@@ -1,7 +1,7 @@
 <?php
 return [
   'app_name' => 'Grupo Ureña Funerarias',
-  'dsn'  => 'mysql:host=ganas001.mysql.guardedhost.com;dbname=ganas001_unitec;charset=utf8mb4',
+  'dsn'  => 'mysql:host=ganas001.mysql.guardedhost.com;dbname=ganas001_urena;charset=utf8mb4',
   'user' => 'ganas001_urena',
   'pass' => 't*H3q2pb6Kk)',
   'modo_libre' => false,
